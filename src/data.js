0dbc9e94818468b619e7f3e735df125017dd3d82
Oluwadolapo Olomo <oluwadolapoolomo@gmail.com>
2022-10-03 13:27:17 -0400

    updates

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export default [ 
     {
-        img: "https://unsplash.com/photos/mou0S7ViElQ",
+        img: "https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
         name: "James King",
         postion:"President and CEO",
         number: "773-114-3033"
 
     },
     {
-        img: "https://unsplash.com/photos/d0oYF8hm4GI",
+        img: "https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1618&q=80",
         name: "Julie Taylor",
         position: "VP of Marketing",
         number: "440-313-7077"
     },
     {
-        img: "https://imgur.com/CVf1gnL",
+        img: "https://images.unsplash.com/photo-1557985594-29f3ad9f5134?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60ttps://imgur.com/CVf1gnL",
         name: "Peter Parker",
         position: "CFO",
-        number: "1-800-SPIDY"
+        number: "1-800-SPIDEY"
     },
     {
-        img: "https://unsplash.com/photos/t0Bv0OBQuTg",
+        img: "https://images.unsplash.com/photo-1628260412297-a3377e45006f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
         name: "Egg Face",
         position: "VP of Engineering",
         number: "240-773-3EGG"
     },
     {
-        img: "https://unsplash.com/photos/sR1BaDlRSKM",
+        img: "https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
         name: "Goku",
         position: "Lead Fighter",
         number: "443-566-FIGHT"
     },
     {
-        img: "https://unsplash.com/photos/sfAEazGOY0M",
+        img: "https://images.unsplash.com/photo-1616098063625-65f32186e609?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         name: "Jessie",
         position: "Cow-girl",
         number: "450-768-RIDE"
